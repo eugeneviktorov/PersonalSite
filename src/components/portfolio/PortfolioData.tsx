2d63eee020708e6fe5b15d63db2f2b0341233456
Eugene <e.viktorovvv@gmail.com>
2024-01-25 12:41:40 +0600

    Система предварительной загрузки изображения

--- src/components/portfolio/PortfolioData.tsx
+++ src/components/portfolio/PortfolioData.tsx
@@ -1,22 +1,35 @@
 import * as React from "react";
 import "./Portfolio.css";
 
-const PortfolioItem = ({ item }: { item: any }) => (
-  <div className="portfolioPhoto">
-    <a href={item.link}>
-      <div className="hideImage">
-        <img
-          src={require(`../../assets/image/${item.image}`)}
-          className="portfolioPhotoSettings"
-          alt={item.title}
-        />
+const PortfolioItem = ({ item }: { item: any }) => {
+  const [imageLoaded, setImageLoaded] = React.useState(false);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
+  return (
+    <div className="portfolioPhoto">
+      <a href={item.link}>
+        <div className="hideImage">
+          <div className={`loadingImage ${imageLoaded ? "hidden" : ""}`}></div>
+          <img
+            src={require(`../../assets/image/${item.image}`)}
+            className={`portfolioPhotoSettings ${
+              imageLoaded ? "visible" : "hidden"
+            }`}
+            alt={item.title}
+            onLoad={handleImageLoad}
+          />
+        </div>
+      </a>
+
+      <div className="portfolioTitleName">
+        <a href={item.link}>{item.title}</a>
       </div>
-    </a>
-    <div className="portfolioTitleName">
-      <a href={item.link}>{item.title}</a>
+      <div className="portfolioTextAbout">{item.description}</div>
     </div>
-    <div className="portfolioTextAbout">{item.description}</div>
-  </div>
-);
+  );
+};
 
 export default PortfolioItem;
