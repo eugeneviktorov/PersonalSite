bf85cc7e58815933c3d07454c782f1de8275118a
Eugene <e.viktorovvv@gmail.com>
2024-02-24 17:26:59 +0600

    Updates

--- projects/ShortStory/src/scripts/scripts.js
+++ projects/ShortStory/src/scripts/scripts.js
@@ -7,22 +7,30 @@ const jsonFile = isIndexPage
 fetch(jsonFile)
   .then((response) => response.json())
   .then((data) => {
-    const faqContainer = document.getElementById("faqAndRulesContainer");
+    const meaningContainer = document.getElementById("meaningContainer");
 
     data.forEach((item, index) => {
-      const faqItem = document.createElement("div");
-      faqItem.classList.add("faqAndRulesQuestion");
-      faqItem.textContent = `${index + 1}. ${item.faqAndRulesQuestion}`;
+      const meaningItem = document.createElement("div");
+      meaningItem.classList.add("meaning");
+      meaningItem.textContent = `${index + 1}. ${item.meaning}`;
 
       const answer = document.createElement("div");
       answer.classList.add("answer");
       answer.innerHTML = item.answer;
 
-      faqItem.addEventListener("click", function () {
-        const isActive = faqItem.classList.contains("active");
+      const icon = document.createElement("img");
+      icon.classList.add("icon");
+      icon.src = "src/icon/angle-up.svg";
+      icon.style.display = "none";
 
-        faqItem.classList.toggle("active", !isActive);
+      meaningItem.appendChild(icon);
+
+      meaningItem.addEventListener("click", function () {
+        const isActive = meaningItem.classList.contains("active");
+
+        meaningItem.classList.toggle("active", !isActive);
         answer.classList.toggle("active", !isActive);
+        icon.style.display = isActive ? "none" : "block";
 
         if (!isActive) {
           answer.style.maxHeight = answer.scrollHeight + "px";
@@ -35,8 +43,8 @@ fetch(jsonFile)
         }
       });
 
-      faqContainer.appendChild(faqItem);
-      faqContainer.appendChild(answer);
+      meaningContainer.appendChild(meaningItem);
+      meaningContainer.appendChild(answer);
     });
   })
   .catch((error) => console.error("Error fetching data:", error));
