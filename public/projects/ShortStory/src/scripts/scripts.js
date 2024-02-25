78e1a72f08d3282ee7058f6378a60d58cbf1c0ff
Eugene <e.viktorovvv@gmail.com>
2024-02-25 12:05:05 +0600

    Фикс проекта

--- public/projects/ShortStory/src/scripts/scripts.js
+++ public/projects/ShortStory/src/scripts/scripts.js
@@ -18,9 +18,9 @@ fetch(jsonFile)
       answer.classList.add("answer");
       answer.innerHTML = item.answer;
 
-      const icon = document.createElement("img");
+      const icon = document.createElement("svg");
       icon.classList.add("icon");
-      icon.src = "src/icon/angle-up.svg";
+      icon.innerHTML = '<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg>';
       icon.style.display = "none";
 
       meaningItem.appendChild(icon);
